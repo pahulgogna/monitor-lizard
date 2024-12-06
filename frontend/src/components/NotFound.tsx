474edce520a57e4121e4b8e71e9802fbe3b4e05e
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-12-06 23:35:59 +0530

    fix: refactor NotFound component to use useEffect for navigation based on token state

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -2,25 +2,32 @@ import { useNavigate } from 'react-router-dom'
 import { useRecoilValueLoadable } from 'recoil'
 import { tokenAtom } from '../store/atom/atom'
 import Loading from './overlays/Loading'
+import { useEffect } from 'react'
 
 function NotFound() {
     const navigate = useNavigate()
     const token = useRecoilValueLoadable(tokenAtom)
+
+    useEffect(() => {
+
+        if(token.state === "hasValue"){
+        
+            if(token.contents){
+                navigate("/dashboard")
+            }
+            else{
+                navigate("/login")
+            }
+        }
+
+    }, [])
 
     if(token.state === "loading"){
         return (
             <Loading/>
         )
     }
-    else if(token.state === "hasValue"){
-        
-        if(token.contents){
-            navigate("/dashboard")
-        }
-        else{
-            navigate("/login")
-        }
-    }
+    
   
 }
 
